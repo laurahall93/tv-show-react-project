3b97725bb5b4358cec5fddb390e01e6a42f073f9
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-24 15:02:49 +0100

    added EpisodeList component

---
+++ src/components/EpisodesList.tsx
@@ -0,0 +1,23 @@
+interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  rating: { average: number };
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
+const EpisodesList = (episodes: IEpisode[]): JSX.Element => {
+  return <p> Episode lists</p>;
+};

fbfdf45d49e99c0c688c238a2a7c44c328280104
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-24 14:45:20 +0100

    formatted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import episodes from './episodes.json'
+import episodes from "./episodes.json";
 
 console.log(`Imported ${episodes.length} episode(s)`);
 console.log(`First episode's name is ${episodes[0].name}`);
-
 
 function App(): JSX.Element {
   return <h1>Cynthias and Lauras Fave TV Show!!!</h1>;

840c1d917b23bc81074d9155835aca83f538b6f9
Laura Hall <podinjshmi1993@gmail.com>
2023-07-24 14:41:49 +0100

    added episodes.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import episodes from './episodes.json'
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
+
+
 function App(): JSX.Element {
   return <h1>Cynthias and Lauras Fave TV Show!!!</h1>;
 }

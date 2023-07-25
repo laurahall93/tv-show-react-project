2a270efabe490ef6cf928b9561350beb00adf8cc
Laura Hall <podinjshmi1993@gmail.com>
2023-07-25 13:28:24 +0100

    added header, mainbody and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import episodes from "./episodes.json";
-import { EpisodesList } from "./components/EpisodesList";
-
-console.log(`Imported ${episodes.length} episode(s)`);
-console.log(`First episode's name is ${episodes[0].name}`);
+import { MainBody } from "./components/MainBody";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App(): JSX.Element {
-  const listEpisode = episodes.map((episode, id) => (
-    <div key={id}>
-      <EpisodesList
-        id={episode.id}
-        url={episode.url}
-        name={episode.name}
-        season={episode.season}
-        number={episode.number}
-        type={episode.type}
-        airdate={episode.airdate}
-        airtime={episode.airtime}
-        airstamp={episode.airstamp}
-        rating={episode.rating}
-        runtime={episode.runtime}
-        image={episode.image}
-        summary={episode.summary}
-        _links={episode._links}
-      />
-    </div>
-  ));
   return (
     <>
-      <h1>Cynthias and Lauras Fave TV Show!!!</h1>
-      {listEpisode}
+      <Header />
+      <MainBody />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(): JSX.Element {
+  return (
+    <div>
+      <h1>Cynthias and Lauras Fave TV Show!!!</h1>
+    </div>
+  );
+}

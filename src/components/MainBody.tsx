2d7e4f116642a194cca3b99b60dbc6bc6d741ddf
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-25 14:22:18 +0100

    added comments for optional props that need fixing

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -9,11 +9,11 @@ export function MainBody(): JSX.Element {
         name={episode.name}
         season={episode.season}
         number={episode.number}
-        rating={episode.rating}
+        rating={episode.rating} //need to remove this and make it optional
         image={episode.image}
         url={episode.url}
         summary={episode.summary}
-        _links={episode._links}
+        _links={episode._links} //need to remove this and make it optional
       />
     </div>
   ));

c6edcabe077776bb7f59bd44bd51c8af83713e12
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-26 16:40:16 +0100

    started implementing search box

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,7 +1,21 @@
 import { EpisodesEntry } from "./EpisodesEntry";
 import episodes from "../episodes.json";
+import { useState } from "react";
 
 export function MainBody(): JSX.Element {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+    console.log(
+      episodes.filter(
+        (episode) =>
+          episode.name ||
+          episode.summary.toLowerCase().includes(event.target.value)
+      )
+    );
+  };
+
   const listEpisode = episodes.map((episode) => (
     <div key={episode.id}>
       <EpisodesEntry
@@ -17,7 +31,12 @@ export function MainBody(): JSX.Element {
 
   return (
     <>
-      <input type = "text" placeholder="search tv show"/>
+      <input
+        type="text"
+        placeholder="search tv show"
+        value={searchInput}
+        onChange={handleChange}
+      />
       <div className="episode-container">{listEpisode}</div>
     </>
   );
